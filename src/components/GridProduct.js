e2efa363d11711593972bf701eb3ca37de092408
Tess Rezard <tessrezard@gmail.com>
2023-10-25 14:30:35 +0100

    styling for grid products

--- src/components/GridProduct.js
+++ src/components/GridProduct.js
@@ -4,25 +4,57 @@ import styles from '../styles/Products.module.css'
 
 function GridProduct({name, description, brief, keyWords, heroImg, pairImg}) {
 
-console.log(heroImg)
+    const sortedKeyWords = keyWords.sort((a, b) => a.length - b.length);
+    console.log(sortedKeyWords);
+    let shortestWords = sortedKeyWords.slice(0, 4);
+    console.log(shortestWords);
+
+    const [word1, word2, word3, word4] = shortestWords;
+    
+    const keyWordsSorted = keyWords.join('✦');
+
+    console.log(keyWordsSorted);
+
     return (
-        <div className={styles.gridProductContainer}>
+        <>
+            <div className={styles.gridProductContainer}>
 
-            <div className={styles.arch}>
+                <div className={styles.keyWordContainer}>
+                    <p className={styles.cornerLEFTStar}>✦</p>
+                    <p className={styles.topLEFTStar}>✦</p>
+                    <p className={` ${styles.keyWordsLeft} ${styles.keyWords} ${styles.topLEFTWord} `}>{word1}</p>
+                    <p className={` ${styles.keyWordsLeft} ${styles.keyWords} ${styles.bottomLEFTWord} `}>{word4}</p>
+                    <p className={styles.bottomStar}>✦</p>
+                </div>
 
-                <div className={styles.imgAndTitle}>
-
-                    <img src={heroImg} className={styles.heroImg} />
-                    <div className={styles.titleContainer}>
-                        <h1 className={styles.title}>{name}</h1>
+                <div className={styles.arch}>
+                    <div className={styles.imgAndTitle}>
+                        <img src={heroImg} className={styles.heroImg} />
+                        <div className={styles.titleContainer}>
+                            <h1 className={styles.title}>{name}</h1>
+                        </div>
                     </div>
+                </div>
+
+                <div className={styles.keyWordContainer}>
+                    <p className={styles.cornerRIGHTStar}>✦</p>
+                    <p className={styles.topRIGHTStar}>✦</p>
+                    <p className={ `${styles.keyWordsRight} ${styles.keyWords} ${styles.topRIGHTWord} `}>{word2}</p>
+                    <p className={ `${styles.keyWordsRight} ${styles.keyWords} ${styles.bottomRIGHTWord} `}>{word3}</p>
+                    <p className={styles.bottomStar}>✦</p>
+                </div>
+
+
+
+
 
                 </div>
 
-            </div>
-            
 
-        </div>
+
+        </>
+        
+        
     )
 }
 
