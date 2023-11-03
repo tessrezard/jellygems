5ce267ffb3fdedd420eb5ec27224522f57a62735
Tess Rezard <tessrezard@gmail.com>
2023-11-03 15:13:53 +0000

    try adding / to path because of the * path on root? maybe?

--- src/App.js
+++ src/App.js
@@ -16,12 +16,12 @@ import Home from './pages/Home';
 const appRouter = createBrowserRouter(createRoutesFromElements( 
   
   <Route path='*' element={ <Root/> } >
-      <Route path='home' element={ <Home/> } />
-      <Route path='products' element={ <Products/> } />
-      <Route path="product-details-page/:stone" element={ <ProductDetailsPage/> } />
-      <Route path="about" element={ <About/> } />
-      <Route path='basket' element={ <Basket/> } />
-      <Route path='checkout' element={ <Checkout/> } />
+      <Route path='/home' element={ <Home/> } />
+      <Route path='/products' element={ <Products/> } />
+      <Route path="/product-details-page/:stone" element={ <ProductDetailsPage/> } />
+      <Route path="/about" element={ <About/> } />
+      <Route path='/basket' element={ <Basket/> } />
+      <Route path='/checkout' element={ <Checkout/> } />
   </Route>
   ));
 
